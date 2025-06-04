9d96077d477ce878a24ef991ba80dba4825435c9
andrjre <andrjre@gmail.com>
2025-06-04 21:35:11 +0100

    Box loop?

--- index.js
+++ index.js
@@ -4,13 +4,25 @@ let play = document.querySelector("#play")
 for(i = 0; i < 9 ; i++){
     let box = document.createElement("div")
     box.className = "box"
-    box.id ="box" +  (i + 1)
+    box.id = "box" +  (i + 1)
     boxes.appendChild(box)
+
 }
+
+let number = 0
+
+for(i = 0; i < 9; i++){
+
+number++
+let boxId = document.querySelector("#box" + number)
+console.log(boxId)
+
+}
+
 
 play.addEventListener("click", function(){
 
-let box = document.querySelector("box" + i)
+let box = document.querySelector("#box" + i)
 box.style.backgroundColor = "grey"
 
 })
