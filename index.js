88fc9be2d73dcd042092351482f3b9fc6d5481ab
andrjre <andrjre@gmail.com>
2025-06-04 21:11:33 +0100

    Grid.

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
 let boxes = document.getElementById("boxes")
-
-function makeBox(){
-
-    let box = document.createElement("div")
-    box.className = "box"
-    boxes.appendChild(box)
-
-}
+let play = document.querySelector("#play")
 
 for(i = 0; i < 9 ; i++){
-    makeBox()
-
-    
+    let box = document.createElement("div")
+    box.className = "box"
+    box.id ="box" +  (i + 1)
+    boxes.appendChild(box)
 }
 
+play.addEventListener("click", function(){
+
+let box = document.querySelector("box" + i)
+box.style.backgroundColor = "grey"
+
+})
